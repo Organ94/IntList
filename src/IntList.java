2a772a33a8eee354b732bba9aab25a77754ada76
Organ94 <pechin1994@mail.ru>
2022-04-05 22:44:58 +0300

    Adding class IntList.java

---
+++ src/IntList.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class IntList {
+
+    List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+    public List<Integer> getIntList() {
+        return intList;
+    }
+}
